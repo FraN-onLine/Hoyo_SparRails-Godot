f30cf3c7eddbf861efb6055952cee344939b065a
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-26 22:02:03 +0800

    Add files via upload

--- spar-rails/global.gd
+++ spar-rails/global.gd
@@ -7,10 +7,16 @@ var selectedMoveType = "typeless"
 var selectedUnit = null
 var selectedEnemy = null
 var units = 0
+var enemies = 0
 var turn = 1
+var level = 0
 
 func _ready() -> void:
 	units = get_tree().get_nodes_in_group("FriendlyUnits").size()
-	
+	enemies = get_tree().get_nodes_in_group("HostileUnits").size()
 func _process(delta: float) -> void:
 	units = get_tree().get_nodes_in_group("FriendlyUnits").size()
+	enemies = get_tree().get_nodes_in_group("HostileUnits").size()
+	
+	if enemies == 0:
+		isPlayerTurn = true
